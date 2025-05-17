00f229d743d085d6cd4a762f7a61db199f6016ba
aiveth <78900881+a1veth@users.noreply.github.com>
2025-05-17 15:58:00 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Go API Template
+
+Template proyek REST API menggunakan Go, Gin, GORM, PostgreSQL, dan JWT.
---
+++ config/config.go
@@ -0,0 +1,23 @@
+package config
+
+import (
+    "fmt"
+    "log"
+    "os"
+
+    "gorm.io/driver/postgres"
+    "gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase() {
+    dsn := os.Getenv("DB_URL")
+    db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+    if err != nil {
+        log.Fatal("Failed to connect to database:", err)
+    }
+
+    fmt.Println("Database connection established")
+    DB = db
+}
